4a834b7e32fe13e8a3e3fae8d5f9aa63efcce850
ikramebakkari <ikrame.bakkari@insa.ueuromed.org>
2023-05-27 16:56:41 +0200

    adding csrf attack protection

--- part2/index.js
+++ part2/index.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const { createReadStream } = require('fs');
-
 const app = express();
 
 const COOKIE_SECRET = 'une_secret_key_au_hasard';
@@ -12,11 +7,13 @@ const USERS = { alice: 'p1', bob: 'p2' };
 const BALENCES = { alice: 500, bob: 1000 };
 
 // Middleware pour parser les cookies
+app.use(cookieParser());
 app.use(cookieParser(COOKIE_SECRET));
 
 // Configuration de la session
 app.use(
     session({
+        secret: 'votre_secret_key',
         secret: COOKIE_SECRET,
         resave: true,
         saveUninitialized: true,
@@ -28,12 +25,11 @@ app.use(
     })
 );
 
-var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-// Page d'accueil
 app.get('/', function (req, res) {
     // Vérifier si l'utilisateur est connecté en vérifiant la présence d'une variable de session
     if (req.session.username) {
+        res.send(`Bienvenue, ${req.session.username} !`);
         const balence = BALENCES[req.session.username];
         res.send(`Bienvenue ${req.session.username}! Vous avez ${balence}$ sur votre compte.
             </br></br>
@@ -82,6 +78,9 @@ app.post('/transfer', (req, res) => {
 
 // Page de connexion
 app.post('/login', (req, res) => {
+    // Simuler le processus d'authentification
+    const username = 'user';
+    const password = 'mdp';
 
     username = req.body.username;
     password = USERS[username];
@@ -90,8 +89,10 @@ app.post('/login', (req, res) => {
     if (req.body.username === username && req.body.password === password) {
         // Définir la variable de session avec le nom d'utilisateur
         req.session.username = req.body.username;
+        res.send('Vous êtes connecté');
         res.redirect('/');
     } else {
+        res.send('Identifiants incorrects');
         res.send('Identifiants incorrects ! Veuillez réessayer. </br> <a href="/">Retour</a>');
     }
 });
@@ -100,10 +101,7 @@ app.post('/login', (req, res) => {
 app.get('/logout', (req, res) => {
     // Détruire la session en supprimant la variable de session
     req.session.destroy();
+    res.redirect('/');
+    res.send('Vous êtes déconnecté');
     res.send('Vous êtes déconnecté. </br> <a href="/">Retour à la page de connexion</a>');
-});
-
-// Démarrer le serveur
-app.listen(3000, () => {
-    console.log('Serveur démarré sur le port 3000');
 });
